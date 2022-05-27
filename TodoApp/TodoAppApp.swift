302f3b294525988c0e6411484483e41caf2d0656
Deniz Can <deniz@monolog.com.tr>
2022-05-27 11:54:08 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Deniz Can on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
